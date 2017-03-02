937fe12d9704ec6839d514c8ad521cb56dde6ac6
Lai Christophe <Lai Christophe@MSI>
2017-03-02 13:46:00 +0300

    Script d'insertion ovaina default de id 2.0 mila verifien lecontrainte ref av mreinitialiser base

--- Mcd/Script d'Insertion.sql
+++ Mcd/Script d'Insertion.sql
@@ -1,14 +1,14 @@
-INSERT INTO client (idclient, nomclient, emailclient, passclient) VALUES (1, 'Christophe', 'lai_christophe@hotmail.com', 'root');
-INSERT INTO client (idclient, nomclient, emailclient, passclient) VALUES (2, 'Alexandra', 'alexandralai@yahoo.fr', 'root');
+INSERT INTO client (idclient, nomclient, emailclient, passclient) VALUES (default, 'Christophe', 'lai_christophe@hotmail.com', 'root');
+INSERT INTO client (idclient, nomclient, emailclient, passclient) VALUES (default, 'Alexandra', 'alexandralai@yahoo.fr', 'root');
 
-INSERT INTO pack_voyage (idpackvoyage, idvoiture, idtarifvoyage, decription) VALUES (3, 1, 1, 'tana tamatave en starex');
-INSERT INTO pack_voyage (idpackvoyage, idvoiture, idtarifvoyage, decription) VALUES (4, 2, 2, 'tana tamatave en patrole');
+INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (default, '4562 Tab', NULL, '150 000,00 €', 'Patrol');
+INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (default, '4895 Tam', NULL, '250 000,00 €', 'Navara');
+INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (default, '8731 Ac', NULL, '200 000,00 €', 'Grand Starex ');
 
-INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (2, '4562 Tab', NULL, '150 000,00 €', 'Patrol');
-INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (3, '4895 Tam', NULL, '250 000,00 €', 'Navara');
-INSERT INTO voiture (idvoiture, matriculevoiture, imagevoiture, prixlocationvoiture, modelvoiture) VALUES (1, '8731 Ac', NULL, '200 000,00 €', 'Grand Starex ');
+INSERT INTO voyage (idvoyage, idpackvoyage, idclient, datedebutvoyage, datefinvoyage) VALUES (default, 4, 5, '2017-02-28', '2017-03-03');
+INSERT INTO voyage (idvoyage, idpackvoyage, idclient, datedebutvoyage, datefinvoyage) VALUES (default, 5, 6, '2017-02-28', '2017-02-03');
 
-INSERT INTO voyage (idvoyage, idpackvoyage, idclient, datedebutvoyage, datefinvoyage) VALUES (3, 3, 1, '2017-02-28', '2017-03-03');
-INSERT INTO voyage (idvoyage, idpackvoyage, idclient, datedebutvoyage, datefinvoyage) VALUES (4, 4, 2, '2017-02-28', '2017-02-03');
+INSERT INTO pack_voyage (idpackvoyage, idvoiture, idtarifvoyage, decription) VALUES (default, 4, 1, 'tana tamatave en starex');
+INSERT INTO pack_voyage (idpackvoyage, idvoiture, idtarifvoyage, decription) VALUES (default, 6, 2, 'tana tamatave en patrole');
 
-INSERT INTO voyage_confirmer (idvoyageconfirmer, idvoyage) VALUES (1, 4);
+INSERT INTO voyage_confirmer (idvoyageconfirmer, idvoyage) VALUES (default, 6);
